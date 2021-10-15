e567e7d752c8569867d5aac98dea5f9e653842b7
chrolss <chrolss@kth.se>
2021-10-15 21:35:53 +0200

    Clean-up of files and code

--- src/data.py
+++ src/data.py
@@ -1,14 +1,10 @@
 import pandas as pd
 from sqlalchemy import create_engine
-from sqlalchemy.sql.elements import quoted_name
 
 def load_from_db(table: str):
-    #engine = create_engine("sqlite:////home/chrolss/PycharmProjects/svpol/data/tweets.db")
     engine = create_engine("sqlite:///data/tweets.db")
     con = engine.connect()
     query = "SELECT * FROM {0}".format(table)
     res = con.execute(query)
 
     return pd.DataFrame(data=res.fetchall(), columns=res.keys())
-
-#df = load_from_db("hashtags")
